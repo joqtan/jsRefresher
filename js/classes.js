635c6b40ce18095f8034aed60d4875ed65a8714b
Joqtan Marmol <joqtanmarmol@gmail.com>
2021-09-07 15:56:36 -0600

    classes

---
+++ js/classes.js
@@ -0,0 +1,26 @@
+class Human {
+  constructor() {
+    this.gender = 'female'
+  }
+
+  printGender() {
+    console.log(this.gender)
+  }
+}
+
+class Person extends Human {
+  constructor() {
+    super()
+    this.name = 'joqtan'
+    this.gender = 'male'
+  }
+  printMyName() {
+    console.log(this.name)
+  }
+}
+
+const person = new Person()
+
+person.printMyName()
+
+person.printGender()

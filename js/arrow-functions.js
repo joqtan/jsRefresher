4e5c8cdacfe0d54000591304ddb67b64b9257eb5
Joqtan Marmol <joqtanmarmol@gmail.com>
2021-09-07 15:30:13 -0600

    arrow functions

---
+++ js/arrow-functions.js
@@ -0,0 +1,19 @@
+function printMyName(name) {
+  console.log(name)
+}
+
+const printMyName2 = (name) => {
+  console.log(name)
+}
+
+// const multiply = (number) => {
+//   return number * 2
+// }
+
+const multiply = (number) => number * 2
+
+printMyName('joqtan')
+
+printMyName2('alberto')
+
+console.log(multiply(2))

b018adfc6957701c14e40080bff96678bd6ba36d
Joqtan Marmol <joqtanmarmol@gmail.com>
2021-09-07 16:14:28 -0600

    classes es6

--- js/classes.js
+++ js/classes.js
@@ -1,20 +1,35 @@
-class Human {
-  constructor() {
-    this.gender = 'female'
-  }
+// class Human {
+//   constructor() {
+//     this.gender = 'female'
+//   }
 
-  printGender() {
+//   printGender() {
+//     console.log(this.gender)
+//   }
+// }
+class Human {
+  gender = 'female'
+
+  printGender = () => {
     console.log(this.gender)
   }
 }
 
+// class Person extends Human {
+//   constructor() {
+//     super()
+//     this.name = 'joqtan'
+//     this.gender = 'male'
+//   }
+//   printMyName() {
+//     console.log(this.name)
+//   }
+// }
 class Person extends Human {
-  constructor() {
-    super()
-    this.name = 'joqtan'
-    this.gender = 'male'
-  }
-  printMyName() {
+  name = 'joqtan'
+  gender = 'male'
+
+  printMyName = () => {
     console.log(this.name)
   }
 }
